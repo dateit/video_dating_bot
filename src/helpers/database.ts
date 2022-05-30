255944f7c64076f668ad47703577a4ed883b8838
bondiano <batbondik0@gmail.com>
2022-05-31 02:02:30 +0600

    Update sentry version

--- src/helpers/database.ts
+++ src/helpers/database.ts
@@ -1,33 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 import { FastifyInstance } from 'fastify';
-import { getCurrentHub } from '@sentry/node';
 
 export const prisma = new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : [],
-});
-
-prisma.$use(async (parameters, next) => {
-  const { model, action, runInTransaction, args } = parameters;
-  const description = [model, action].filter(Boolean).join('.');
-  const data = {
-    model,
-    action,
-    runInTransaction,
-    args,
-  };
-
-  const transaction = getCurrentHub().getScope().getTransaction();
-
-  const span = transaction?.startChild({
-    op: 'db',
-    description,
-    data,
-  });
-
-  const result = await next(parameters);
-  span?.finish();
-
-  return result;
 });
 
 export const initDatabase = async (app: FastifyInstance) => {
